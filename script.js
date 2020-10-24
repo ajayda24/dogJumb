d637aa10f2873281ef3c071d85222f88f15752e1
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2020-10-24 12:38:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var sWidth = window.innerWidth;
+var sHeight = window.innerHeight;
+
+var gameScreen = document.getElementById("game");
+gameScreen.style.height = sHeight+"px";
+gameScreen.style.width = sWidth+"px";
+
+//Dog Position
+var dogDiv = document.getElementById("dogDiv");
+dogDiv.style.top = (sHeight-108)+"px";
+
+//Ring Position
+var ringDiv = document.getElementById("ringDiv");
+ringDiv.style.left = (sWidth-(sWidth-150))+"px";
